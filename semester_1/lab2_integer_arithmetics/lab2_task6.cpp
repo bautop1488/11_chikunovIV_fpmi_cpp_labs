cbf8ff509e68e164d1b3bbec2fa305ea10d3c5ce
bautop1488 <ivanchikunov123@gmail.com>
2025-10-14 10:29:09 +0300

    optimization

--- semester_1/lab2_integer_arithmetics/lab2_task6.cpp
+++ semester_1/lab2_integer_arithmetics/lab2_task6.cpp
@@ -2,35 +2,30 @@
 #include <cmath>
 
 bool is_prime(int x){
-    int d = 0;
-    for(int i=1; i <= x;i++){
+    int dividers = 0;
+    for(int i=2; i <= sqrt(x)+1;i++){
         if(x % i == 0){
-            d++;
+            dividers++;
         }
     }
-    if(d==2){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return dividers==0;
 }
 
 int main(){
-
-
     int n;
-    std::cout << "type n: ";
-    std::cin >> n;
 
-    if(!std::cin >> n){
+    std::cout << "type n: ";
+
+    if(!(std::cin >> n)){
         std::cout << "Invalid input, error";
         std::exit(1);
     }
 
     std::cout << "all k's: " << std::endl;
 
-    for(int i=1;pow(2, i)-1 <= n;i++){
+    int l = log2(n);
+    //std::cout << l << ' ';
+    for(int i=1;i <= l;i++){
         if(is_prime(i)){
             std::cout << pow(2, i)-1 << std::endl;
         }
